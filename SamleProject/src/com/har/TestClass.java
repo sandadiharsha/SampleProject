d3d277fead13afd442b897daa3dd9e8b904ce874
Harsha <sandadi.harsha@gmail.com>
2014-12-30 13:53:35 +0530

    Sample Project to learn Git.

---
+++ SamleProject/src/com/har/TestClass.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.har;
+
+/**
+ * @author vsa-jan08
+ * 
+ */
+public class TestClass {
+	public static void main(String[] args) {
+		System.out.println("Sample file.");
+	}
+}
